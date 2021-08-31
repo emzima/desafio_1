15f31c56ffcbe33b57c220c1b0b035367004bde5
emzima <ezapata.bcn@gmail.com>
2021-08-31 00:32:29 -0300

    v1

---
+++ ccsjs/main.js
@@ -0,0 +1,11 @@
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Gracias dios';
+ 
+function miFuncion1(){
+  var texto="Gracias por haberme compartido tu lectura,  \nSos muy amable, mi escritura lo acgradece";
+  var gracias = document.getElementsByClassName("gracias");
+  alert(texto);}
+
+function miFuncion2(){
+  var nuevotexto = document.getElementById("porque");
+    nuevotexto.innerHTML = "... tropece con codo a codo que me daba esa oportunidad y cuya reputación le bien precedia.Y aca estamos.";}
